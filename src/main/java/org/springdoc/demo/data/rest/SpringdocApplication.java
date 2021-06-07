29ffaa49ebcb40395e0b45a3c209272d5f9c1b6b
bnasslahsen <badr.nasslashen@gmail.com>
2021-06-07 22:49:21 +0200

    project update

--- src/main/java/org/springdoc/demo/data/rest/SpringdocApplication.java
+++ src/main/java/org/springdoc/demo/data/rest/SpringdocApplication.java
@@ -16,14 +16,12 @@
  *
  */
 
-package org.springdoc.demo.app3;
+package org.springdoc.demo.data.rest;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.GroupedOpenApi;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -31,34 +29,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class WebfluxDemoApplication {
+public class SpringdocApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebfluxDemoApplication.class, args);
+		SpringApplication.run(SpringdocApplication.class, args);
 	}
 
 	@Bean
 	public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
 		return new OpenAPI()
-				.components(new Components().addSecuritySchemes("basicScheme",
-						new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
-				.info(new Info().title("Tweet API").version(appVersion)
+				.components(new Components())
+				.info(new Info().title("Twitter API").version(appVersion)
 						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
-
-	@Bean
-	public GroupedOpenApi storeOpenApi() {
-		String[] paths = { "/tweets/**" };
-		return GroupedOpenApi.builder().group("tweets").pathsToMatch(paths)
-				.build();
-	}
-
-	@Bean
-	public GroupedOpenApi userOpenApi() {
-		String[] paths = { "/stream/**" };
-		String[] packagedToMatch = { "org.springdoc.demo.app3" };
-		return GroupedOpenApi.builder().group("x-stream").pathsToMatch(paths).packagesToScan(packagedToMatch)
-				.build();
-	}
-
 }
