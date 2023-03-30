c717bfdba5b6914130b6b849e6d3372f999f70d7
bnasslahsen <badr.nasslashen@gmail.com>
2023-03-30 19:48:55 +0200

    code review

--- src/main/deployment/deploy-app.sh
+++ src/main/deployment/deploy-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # DB DEPLOYMENT
-kubectl replace --force -f twitter-service.yml
+kubectl replace --force -f ./twitter-service.yml
 if ! kubectl wait deployment twitter-service --for condition=Available=True --timeout=120s
   then exit 1
 fi
